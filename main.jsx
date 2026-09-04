fad451db78bfc1677ae64cc85fce805304e5705e
yf7526-source <yf7526@gmail.com>
2026-09-04 13:40:55 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
-import '@/index.css'
-import { registerPwa } from '@/lib/pwa'
-import { startProductionDiagnostics } from '@/lib/productionDiagnostics'
+import './index.css'
+import { registerPwa } from './lib/pwa'
+import { startProductionDiagnostics } from './lib/productionDiagnostics'
 import AppErrorBoundary from '@/components/AppErrorBoundary'
 
 startProductionDiagnostics()
